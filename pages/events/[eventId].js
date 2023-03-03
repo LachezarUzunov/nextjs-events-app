9283141166a57f3ec00578cc0275afb6af03f23a
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-03-03 18:56:03 +0200

    alter component, completing project

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import EventSummary from "@/components/event-detail/event-summary";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventContent from "@/components/event-detail/event-content";
+import ErrorAlert from "@/components/ui/error-alert";
 
 const EventDetailPage = () => {
   const router = useRouter();
@@ -11,7 +12,11 @@ const EventDetailPage = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No Event Found!</p>;
+    return (
+      <ErrorAlert>
+        <p>No Event Found!</p>
+      </ErrorAlert>
+    );
   }
   return (
     <Fragment>
