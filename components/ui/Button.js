ce0888432498c6452c38d4d8d5a4620d91acb016
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-03-03 15:43:37 +0200

    adding search events component

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -3,11 +3,19 @@ import classes from "./Button.module.css";
 import Link from "next/link";
 
 const Button = (props) => {
-  return (
-    <Link className={classes.btn} href={props.link}>
-      {props.children}
-    </Link>
-  );
+  if (props.link) {
+    return (
+      <Link className={classes.btn} href={props.link}>
+        {props.children}
+      </Link>
+    );
+  } else {
+    return (
+      <button className={classes.btn} onClick={props.onClick}>
+        {props.children}
+      </button>
+    );
+  }
 };
 
 export default Button;
