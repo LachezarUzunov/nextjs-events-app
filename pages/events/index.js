e714bcc8fb27fca0a16c2ad4b3f4b79bad0664c5
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-03-05 07:47:07 +0200

    fetching and filtering events from firebase

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,12 @@
 import { Fragment } from "react";
-import { useRouter } from "next/router";
-import { getAllEvents } from "@/dummy-data";
+import { getAllEvents } from "@/helpers/api-util";
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
+import { useRouter } from "next/router";
 
-const Events = () => {
-  const events = getAllEvents();
+const Events = (props) => {
+  const events = props.events;
   const router = useRouter();
-
   const findEventsHandler = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
 
@@ -20,5 +19,16 @@ const Events = () => {
     </Fragment>
   );
 };
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
 
 export default Events;
